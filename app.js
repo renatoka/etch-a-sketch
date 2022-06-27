8fd8828e0d3134fcf79d64522d51be69303aa96e
Renato <rkauric@vvg.hr>
2022-06-27 18:16:59 +0200

    Fix RGB to randomly color each div upon hover

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ const items = document.querySelectorAll('.item');
 let defaultColor;
 let rgbColor;
 
+/* function randomColor() {
+    rgbColor = '#'+(Math.random().toString(16)+'00000').slice(2,8);
+} */
+
 function randomColor() {
-    rgbColor = '#' + (Math.random().toString(16) + '00000').slice(2, 8);
-    return rgbColor;
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+        color.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + color.join(', ') + ')';
 }
 
 buttons.forEach((button) => {
@@ -26,16 +33,14 @@ buttons.forEach((button) => {
         }
 
         else if (button.className == 'rgb-color') {
-            randomColor();
-            defaultColor = rgbColor;
+            defaultColor = randomColor();
         }
 
     });
-});
 
-
-items.forEach((item) => {
-    item.addEventListener("mouseover", function () {
-        item.style.backgroundColor = defaultColor;
+    items.forEach((item) => {
+        item.addEventListener("mouseover", function () {
+            item.style.backgroundColor = defaultColor;
+        });
     });
-}); 
+});
